f2f40b70fdc6f8d1c00f2217de194518b793f196
Jordan Williams <jordanw1@iastate.edu>
2018-02-08 17:09:16 -0600

    no message

--- OpenCVTest/HW2.cpp
+++ OpenCVTest/HW2.cpp
@@ -11,6 +11,8 @@ int main(int argc, char** argv) {
 	//board_rgb = imread($(ProjectDir) + "  ", 1);
 	se_rgb = imread("../../images/frame.png", 1);
 
+	int boxSize = 60;
+
 	cvtColor(board_rgb, board_g, cv::COLOR_BGR2GRAY);
 	threshold(board_g, board_bin, 0.85 * 255, 255, THRESH_BINARY);
 
@@ -44,20 +46,70 @@ int main(int argc, char** argv) {
 
 	findContours(board_dilated, spaces, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-	Mat contours = Mat::ones(board_dilated.size(), CV_8UC3);
+	Mat contours = Mat::zeros(board_dilated.size(), CV_8UC3);
 	Mat black = Mat::zeros(board_dilated.size(), CV_8UC3);
+	Mat contours2 = Mat::zeros(board_dilated.size(), CV_8UC3);
 
+	int maxX, maxY;
 
+	for (int i = 0; i < spaces.size(); i++) {
+		
+		printf("contour %i size: %i\n", i, spaces[i].size() );
+
+		if (spaces[i][0].x == 0 && spaces[i][0].y == 0) {
+			printf("dfkdjfd\n");
+			spaces.erase(spaces.begin() + i);
+		}
+	}
+
+	/*
 	for (int i = 0; i < spaces.size(); i++)
 	{
 		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
 		drawContours(contours, spaces, i, color, 2, 8, hierarchy, 0, Point());
 		printf("size: %f\n", spaces[i].size());
 	}
+	*/
+	/// Get the moments
+	std::vector<Moments> mu(spaces.size());
+	for (int i = 0; i < spaces.size(); i++)
+	{
+		mu[i] = moments(spaces[i], false);
+	}
+
+	///  Get the mass centers:
+	std::vector<Point2f> mc(spaces.size());
+	for (int i = 0; i < spaces.size(); i++)
+	{
+		mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
+	}
+	Vec2i recSize(boxSize, boxSize);
+
+	Scalar color = Scalar(255, 0, 0);
+	Scalar color2 = Scalar(10, 150, 70);
+	for (int i = 0; i< spaces.size(); i++)
+	{
+		//Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+		//drawContours(contours, spaces, i, color, 2, 8, hierarchy, 0, Point());
+		//circle(contours, mc[i], 4, color, -1, 8, 0);
+
+		Vec2i recPos(mc[i].x, mc[i].y);
+
+		rectangle(contours, recPos - recSize, recPos + recSize, color, -1, 9, 0);
+		rectangle(contours2, recPos - recSize, recPos + recSize, color2, -1, 9, 0);
+	}
+	Mat colFlip;
+	flip(contours2, colFlip, 1);
+
+
+	Mat dest;
+	addWeighted(colFlip, 0.9, contours, 0.9, 0.0, dest);
 
 	/// Show in a window
-	namedWindow("Contours", CV_WINDOW_AUTOSIZE);
-	imshow("Contours", contours);
+	//namedWindow("Contours", CV_WINDOW_AUTOSIZE);
+	//imshow("Contours", contours);
+	//imshow("Contours 2", colFlip);
+	imshow("mix", dest);
 
 	imshow("Display Image", board_bin);
 	imshow("2", se_bin);
